509f2494471d1507e16bd62bf188b7cca9c10413
Nassim KRIFA <nkrifa@e1r10p2.42.fr>
2018-04-10 07:18:01 +0200

    bref

---
+++ Makefile
@@ -0,0 +1,52 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: nkrifa <nkrifa@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/10/09 13:47:47 by nkrifa            #+#    #+#              #
+#    Updated: 2018/04/10 04:06:42 by nkrifa           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = fdf
+CC = clang
+LIB_NAME = ft
+LIBDIR = libft
+FLAGS = -g -Wall -Wextra -Werror -O3
+
+HEADERS = includes/fdf.h
+
+SOURCES = srcs/draw.c \
+		  srcs/tools.c \
+		  srcs/parsing.c \
+		  srcs/main.c \
+		  srcs/controls.c
+	
+
+OBJECTS = $(SOURCES:.c=.o)
+
+$(NAME): $(LIBDIR)/lib$(LIB_NAME).a $(OBJECTS)
+	@$(CC) $(FLAGS) -L $(LIBDIR) -l $(LIB_NAME) -o $@ $^ -lmlx -framework OpenGL -framework AppKit
+	@echo "$(NAME) is ready  to use"
+
+$(LIBDIR)/lib$(LIB_NAME).a : 
+	@$(MAKE) -C $(LIBDIR)
+
+all: $(NAME)
+
+%.o: %.c $(HEADERS)/$(NAME).h
+	@$(CC) $(FLAGS) -I $(HEADERS) -c $< -o $@
+
+clean:
+	@rm -f $(OBJECTS)
+	@make clean -C $(LIBDIR)
+
+fclean: clean
+	@rm -f $(NAME)
+	@make fclean -C $(LIBDIR)
+
+re: fclean $(NAME)
+
+.PHONY: all, clean, fclean, re
---
+++ libft/ft_strjoin.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsidler <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/21 18:49:01 by fsidler           #+#    #+#             */
+/*   Updated: 2016/02/02 19:23:29 by fsidler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*strjoin;
+
+	if (!s1)
+		return (NULL);
+	strjoin = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!strjoin)
+		return (NULL);
+	ft_strcpy(strjoin, s1);
+	ft_strcat(strjoin, s2);
+	return (strjoin);
+}
+
+char	*ft_strjoinandfree1(char *s1, char *s2)
+{
+	char	*strjoin;
+
+	if (!s1)
+		return (NULL);
+	strjoin = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!strjoin)
+		return (NULL);
+	ft_strcpy(strjoin, s1);
+	ft_strcat(strjoin, s2);
+	ft_strdel(&s1);
+	ft_strdel(&s2);
+	return (strjoin);
+}
+
+char	*ft_strjoinandfree2(char *s1, char *s2)
+{
+	char	*strjoin;
+
+	if (!s1)
+		return (NULL);
+	strjoin = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!strjoin)
+		return (NULL);
+	ft_strcpy(strjoin, s1);
+	ft_strcat(strjoin, s2);
+	ft_strdel(&s1);
+	return (strjoin);
+}
+
+char	*ft_strjoinandfree3(char *s1, char *s2)
+{
+	char	*strjoin;
+
+	if (!s1)
+		return (NULL);
+	strjoin = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!strjoin)
+		return (NULL);
+	ft_strcpy(strjoin, s1);
+	ft_strcat(strjoin, s2);
+	ft_strdel(&s2);
+	return (strjoin);
+}
